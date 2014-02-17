e84a5f3c68b4ba3a665d2d9b0a51b4d71c3062ff
Levin <mamboer@live.com>
2014-02-17 16:41:55 +0800

    use http instead of https

--- js/bootup.js
+++ js/bootup.js
@@ -1,8 +1,27 @@
-(function($,R,D){
+/*!
+  * $script.js JS loader & dependency manager
+  * https://github.com/ded/script.js
+  * (c) Dustin Diaz 2014 | License MIT
+  */
+(function(e,t,n){typeof module!="undefined"&&module.exports?module.exports=n():typeof define=="function"&&define.amd?define(n):t[e]=n()})("$script",this,function(){function p(e,t){for(var n=0,r=e.length;n<r;++n)if(!t(e[n]))return i;return 1}function d(e,t){p(e,function(e){return!t(e)})}function v(e,t,r){function y(e){return e.call?e():f[e]}function b(){if(!--g){f[u]=1,o&&o();for(var e in c)p(e.split("|"),y)&&!d(c[e],y)&&(c[e]=[])}}e=e[s]?e:[e];var i=t&&t.call,o=i?t:r,u=i?e.join(""):t,g=e.length;return setTimeout(function(){d(e,function(e){if(e===null)return b();if(h[e])return u&&(l[u]=1),h[e]==2&&b();h[e]=1,u&&(l[u]=1),m(!n.test(e)&&a?a+e+".js":e,b)})},0),v}function m(n,r){var s=e.createElement("script"),a=i;s.onload=s.onerror=s[u]=function(){if(s[o]&&!/^c|loade/.test(s[o])||a)return;s.onload=s[u]=null,a=1,h[n]=2,r()},s.async=1,s.src=n,t.insertBefore(s,t.firstChild)}var e=document,t=e.getElementsByTagName("head")[0],n=/^https?:\/\//,r="string",i=!1,s="push",o="readyState",u="onreadystatechange",a,f={},l={},c={},h={};return v.get=m,v.order=function(e,t,n){(function r(i){i=e.shift(),e.length?v(i,r):v(i,t,n)})()},v.path=function(e){a=e},v.ready=function(e,t,n){e=e[s]?e:[e];var r=[];return!d(e,function(e){f[e]||r[s](e)})&&p(e,function(e){return f[e]})?t():!function(e){c[e]=c[e]||[],c[e][s](t),n&&n(r)}(e.join("|")),v},v.done=function(e){v([null],e)},v})
 
-var cbk = function($s){
-    var prefix = "http://oxox.io/wgdata/js/",
-            t = new Date().getTime();
+;(function($,R,D,$s){
+
+
+var t = new Date().getTime();
+
+var loadJQ = function(cbk){
+    if( typeof($)==='undefined' ){
+        $s('http://static.paipaiimg.com/js/jquery.1.8.js?t='+t,function(){
+            cbk(window['jQuery']);
+        });
+    }else{
+        cbk($);
+    };
+};
+
+var cbk = function(){
+    var prefix = "http://rawgithub.com/oxox/wgdata/master/js/";
     $s([prefix+"base.js?t="+t,
         "http://oxox.io/jq/oxmenu/jquery.oxtree.js?t="+t,
         "http://oxox.io/jq/oxi18n/jquery.oxi18n.js?t="+t,
@@ -26,19 +45,6 @@ var cbk = function($s){
     });
 };
 
-if( typeof(R) =='function' && typeof(D)=='function' && D.amd ){
-    //requirejs style
-    R(['http://oxox.io/assets/js/libs/script.js'],function($s){
-        cbk($s);
-    });
-}else{
-    $.getScript('http://oxox.io/assets/js/libs/script.js')
-        .done(function(js,txtStatus,jqxhr){
-            cbk(window["$script"]);
-        })
-        .fail(function(jqxhr,cfg,err){
-            console.log('Error happening at loading script.js:'+err);
-        });
-};
+loadJQ(cbk);
 
-})(jQuery,window["require"],window["define"]);
+})(jQuery,window["require"],window["define"],window["$script"]);
--- js/xdemo.cfg.js
+++ js/xdemo.cfg.js
@@ -1,8 +1,8 @@
 {
   "name": "网购数据统计",
-  "version": "1.0.0",
-  "css": ["https://rawgithub.com/oxox/wgdata/master/css/style.css"],
-  "js": ["https://rawgithub.com/oxox/wgdata/master/js/bootup.js"],
+  "version": "1.0.1",
+  "css": ["http://rawgithub.com/oxox/wgdata/master/css/style.css"],
+  "js": ["http://rawgithub.com/oxox/wgdata/master/js/bootup.js"],
   "matches": ["http://*.wanggou.com/*"],
   "ename": "ecd.wanggou.xdata",
   "author": "levinhuang",
