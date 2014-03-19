666e3eb6bb3c77f45f4a279c2b906c11bc47df09
Waileung <waileung@qq.com>
2014-03-19 19:18:57 +0800

    针对paipai域

--- js/xdemo.cfg.js
+++ js/xdemo.cfg.js
@@ -3,7 +3,8 @@
   "version": "1.0.1",
   "css": ["http://rawgithub.com/oxox/wgdata/master/css/style.css"],
   "js": ["http://rawgithub.com/oxox/wgdata/master/js/bootup.js"],
-  "matches": ["http://*.wanggou.com/*"],
+  //"matches": ["http://*.wangou.com/*"],
+  "matches": ["http://*.paipai.com/*"],
   "ename": "ecd.wanggou.xdata",
   "author": "levinhuang",
   "descrition": "网购数据统计"
